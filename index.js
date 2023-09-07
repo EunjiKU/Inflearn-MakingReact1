c84374a6551c4aa85ae4ff52c58ea4a32a0ebf79
KUEUNJI <112163447+EunJiKU8151@users.noreply.github.com>
2023-09-07 11:52:47 +0900

    로그인 Auth 기능 만들기

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
+const { auth } = require('./middleware/auth');
 const { User } = require("./models/User");
 
 // client에서 오는 정보를 분석해서 가져옴
@@ -24,7 +25,7 @@ app.get('/', (req, res) => {
   res.send('Hello Worlds!')
 })
 
-app.post('/register',(req,res)=>{   
+app.post('api/users/register',(req, res)=>{   
   //회원가입할 때 필요한 정보들을 client에서 가져오면,
   //그 정보들을 DB에 넣어준다.
   const user = new User(req.body);
@@ -39,7 +40,7 @@ app.post('/register',(req,res)=>{
   });
 })
 
-app.post('/login', (req, res) => {
+app.post('api/users/login', (req, res) => {
   // 요청된 이메일을 데이터베이서 있는지 찾는다
   User.findOne({ email: req.body.email })
     .then((user) => {
@@ -69,6 +70,22 @@ app.post('/login', (req, res) => {
     .catch((err) => {
       return res.status(400).send(err);
     })
+})
+
+app.get('api/users/auth', auth, (req, res) => {
+  // 여기까지 미들웨어를 통과해 왔다는 얘기는
+  // Authentication이 True라는 말
+  res.status(200).json({
+    _id: req.user._id,
+    // 0이면 일반유저, 0이 아니면 관리자~
+    isAdmin: req.user.role === 0 ? false: true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
 })
 
 app.listen(port, () => {
